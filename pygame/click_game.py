7a20ea80e6a5da733478f46ac7d1f74e7f7c62e9
Tajb092 <113620141+Tajb092@users.noreply.github.com>
2023-02-09 12:18:12 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,10 +1,23 @@
-my_character = Actor("Akbar")
+my_character = Actor("akbar-fighting stance")
 my_character.pos = 100, 50
 
 WIDTH = 500
 HEIGHT = my_character.height + 20
 
 def draw():
-    screem.fill((0,0,0))
+    screen.fill((221,221,221))
     screen.clear()
     my_character.draw()
+
+def update():
+    my_character.left += 2
+    if my_character.left > WIDTH:
+        my_character.right = 0
+
+def on_mouse_down(pos):
+    if my_character.collidepoint(pos):
+        print("hi you click!")
+    else:
+        print("not cool dude!")
+
+
